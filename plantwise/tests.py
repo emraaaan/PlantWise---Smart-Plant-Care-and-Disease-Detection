ae8357c7f1220e2d0941d3241fb6fda419dbb1af
arpitajuthi-stack <arpita.juthi@northsouth.edu>
2026-04-30 12:25:49 +0600

    Add files via upload

---
+++ plantwise/tests.py
@@ -0,0 +1,16 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class SimpleUserTest(TestCase):
+
+    def test_user_creation(self):
+        user = User.objects.create_user(
+            username="testuser",
+       
+            password="testpass123"
+        )
+
+        self.assertEqual(user.username, "testuser")
+        self.assertTrue(user.check_password("testpass123"))
